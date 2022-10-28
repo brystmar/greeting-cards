b058ec57fdf66a99e9ab94aec620de4e94884835
Thomas <brystmar@users.noreply.github.com>
2022-10-28 12:43:02 -0700

    Struggling to get Household id auto-assigned by the database for new records

--- routes/household.py
+++ routes/household.py
@@ -9,9 +9,6 @@ import json
 
 logger = getLogger()
 
-# Initialize a parser for the request parameters
-parser = reqparse.RequestParser(trim=True)
-
 
 class HouseholdCollectionApi(Resource):
     """
@@ -70,21 +67,34 @@ class HouseholdApi(Resource):
         logger.debug(f"Start of HouseholdAPI.GET")
         logger.debug(request)
 
+        # Initialize a parser for the request parameters
+        parser = reqparse.RequestParser(trim=True)
+
         # Define the parameters used by this endpoint
-        parser.add_argument("household_id", type=int, nullable=False, store_missing=False,
-                            required=True)
+        parser.add_argument("id", type=int, nullable=False, store_missing=False, location='form')
 
         # Parse the provided arguments
-        args = parser.parse_args()
-        logger.debug(f"Args parsed successfully: {args.__str__()}")
-
-        # Validate that a household_id was provided
         try:
-            household_id = args["households"]
-            logger.debug(f"Household_id={household_id} was read successfully")
+            logger.debug(f"Attempting to parse args...")
+            args = parser.parse_args()
+            if len(args.items()) > 0:
+                logger.debug(f"Args parsed successfully: {args.__str__()}")
+            else:
+                raise KeyError("No household id provided")
         except KeyError as e:
-            logger.info(f"Error parsing household_id: no value was provided. {e}")
-            return f"Must provide a value for household_id.", 400
+            logger.info(f"Error parsing request: {e}")
+            return f"Must provide a household id.", 400
+        except BaseException as e:
+            logger.info(f"Error parsing args: {e}")
+            return f"Error parsing args: {e}", 400
+
+        # Validate that a household id was provided
+        try:
+            household_id = args["id"]
+            logger.debug(f"Household with id={household_id} was read successfully")
+        except KeyError as e:
+            logger.info(f"Error parsing household id: no value was provided. {e}")
+            return f"Must provide a household id.", 400
 
         # Retrieve the selected record
         try:
@@ -92,18 +102,19 @@ class HouseholdApi(Resource):
 
             if household:
                 # Record successfully returned from the db
-                logger.info(f"Found the requested household: {household.to_dict()}")
+                logger.info(f"Found the requested household!")
+                logger.debug(f"Household: {household.to_dict()}")
                 logger.debug("End of HouseholdAPI.GET")
                 return household.to_dict(), 200
             else:
                 # No record with this id exists in the db
-                logger.debug(f"No records found for household_id={household_id}.")
+                logger.info(f"No household found with id={household_id}.")
                 logger.debug("End of HouseholdAPI.GET")
-                return f"No records found for household_id={household_id}.", 404
+                return f"No household found with id={household_id}.", 404
 
         except (InvalidRequestError, NoResultFound, AttributeError) as e:
-            error_msg = f"No records found for household_id={household_id}.\n{e}"
-            logger.debug(error_msg)
+            error_msg = f"No household found with id={household_id}.\n{e}"
+            logger.info(error_msg)
             logger.debug(f"End of HouseholdAPI.GET")
             return error_msg, 404
 
@@ -111,28 +122,50 @@ class HouseholdApi(Resource):
     def post() -> json:
         """Add a new household record to the database"""
         logger.debug(f"Start of HouseholdAPI.POST")
-        logger.debug(request)
+        # logger.debug(f"Request: {request}")
+        # logger.debug(f"Request body: {request.args.to_dict()}")
+        # logger.debug(f"Request json: {request.json}")
+        # logger.debug(f"request.get_json: {request.get_json()}")
+
+        # Initialize a parser for the request parameters
+        parser = reqparse.RequestParser(trim=True)
 
         # Define the parameters used by this endpoint
-        parser.add_argument("nickname", type=str)
-        parser.add_argument("surname", type=str)
-        parser.add_argument("formal_name", type=str)
-        parser.add_argument("relationship", type=str)
-        parser.add_argument("relationship_type", type=str)
+        parser.add_argument("nickname", type=str, location='form')
+        parser.add_argument("first_names", type=str, location='form')
+        parser.add_argument("surname", type=str, location='form')
+        parser.add_argument("formal_name", type=str, location='form')
+        parser.add_argument("relationship", type=str, location='form')
+        parser.add_argument("relationship_type", type=str, location='form')
+        parser.add_argument("family_side", type=str, location='form')
+        parser.add_argument("kids", type=str, location='form')
+        parser.add_argument("pets", type=str, location='form')
+        parser.add_argument("should_receive_holiday_card", type=int, location='form')
+        parser.add_argument("notes", type=str, location='form')
+
+        logger.debug(f"Parser init: {parser.__str__()}")
 
         # Parse the arguments provided
         args = parser.parse_args()
-        logger.debug(f"Args parsed successfully: {args.__str__()}")
+
+        logger.debug(f"should receive? {args.should_receive_holiday_card}")
+        # logger.debug(f"Args parsed successfully: {args.__str__()}")
+        logger.debug(f"Args parsed successfully: {args}")
+
+        logger.debug(f"Args str: {args.__str__()}")
 
         # Create a new Household record using the provided data
         try:
             logger.debug(f"Attempting to create a Household from the args.")
-            new_household = Household(**args.__str__())
+            empty_hh = Household()
+            logger.debug(f"emptyHH: {empty_hh}")
+            logger.debug(f"emptyHH to_dict: {empty_hh.to_dict()}")
+            new_household = Household(**args)
             logger.info(f"New record successfully created: {new_household.to_dict()}")
 
             # Commit this new record so the db generates an id
             logger.debug("Attempting to commit data")
-            db.session.commit()
+            # db.session.commit()
             logger.debug("Commit completed")
 
             # Return the household_id to the requester
@@ -151,12 +184,21 @@ class HouseholdApi(Resource):
         logger.debug(f"Start of HouseholdAPI.PUT")
         logger.debug(request)
 
+        # Initialize a parser for the request parameters
+        parser = reqparse.RequestParser(trim=True)
+
         # Define the parameters used by this endpoint
         parser.add_argument("nickname", type=str)
+        parser.add_argument("first_names", type=str)
         parser.add_argument("surname", type=str)
         parser.add_argument("formal_name", type=str)
         parser.add_argument("relationship", type=str)
         parser.add_argument("relationship_type", type=str)
+        parser.add_argument("family_side", type=str)
+        parser.add_argument("kids", type=str)
+        parser.add_argument("pets", type=str)
+        parser.add_argument("should_receive_holiday_card", type=int)
+        parser.add_argument("notes", type=str)
 
         # Parse the arguments provided
         args = parser.parse_args()
@@ -201,8 +243,11 @@ class HouseholdApi(Resource):
         logger.debug(f"Start of HouseholdAPI.DELETE")
         logger.debug(request)
 
+        # Initialize a parser for the request parameters
+        parser = reqparse.RequestParser(trim=True)
+
         # Define the parameters used by this endpoint
-        parser.add_argument("household_id", type=int, nullable=False, store_missing=False,
+        parser.add_argument("id", type=int, nullable=False, store_missing=False,
                             required=True)
 
         # Parse the provided arguments
@@ -236,13 +281,13 @@ class HouseholdApi(Resource):
                 return household.to_dict(), 200
             else:
                 # No record with this id exists in the db
-                error_msg = f"No record found for household_id={household_id}."
+                error_msg = f"No household found with id={household_id}."
                 logger.debug(error_msg)
                 logger.debug("End of HouseholdAPI.GET")
                 return error_msg, 404
 
         except (InvalidRequestError, NoResultFound, AttributeError) as e:
-            error_msg = f"No record found for household_id={household_id}.\n{e}"
+            error_msg = f"No household found with id={household_id}.\n{e}"
             logger.debug(error_msg)
             logger.debug(f"End of HouseholdAPI.GET")
             return error_msg, 404
