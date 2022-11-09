bc54628013ae71ed794aeffd7febcda65a1cd297
Thomas <brystmar@users.noreply.github.com>
2022-11-08 22:18:20 -0800

    Re-refactored household routes to use flask-restful again

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ app = create_app()
 # Import our routes
 # Must be done after the app is initialized to avoid circular dependencies
 from routes.address import AddressCollectionApi, AddressApi
-from routes.household import api_household_one, api_household_all
+from routes.household import HouseholdCollectionApi, HouseholdApi
 from routes.event import EventCollectionApi, EventApi
 from routes.gift import GiftCollectionApi, GiftApi
 from routes.card import CardCollectionApi, CardApi
 
 # Enable CORS logging
-getLogger('flask_cors').level = DEBUG
+# getLogger('flask_cors').level = DEBUG
 
 # Since this will only ever be a locally-run app, allow CORS for all domains on all routes
 # https://flask-cors.readthedocs.io/en/latest/
@@ -42,6 +42,8 @@ logger.info("Initialized the API for this Flask app")
 logger.debug("Adding our functional endpoints to the API")
 api.add_resource(AddressApi, "/api/v1/address")
 api.add_resource(AddressCollectionApi, "/api/v1/all_addresses")
+api.add_resource(HouseholdApi, "/api/v1/household")
+api.add_resource(HouseholdCollectionApi, "/api/v1/all_households")
 api.add_resource(EventApi, "/api/v1/event")
 api.add_resource(EventCollectionApi, "/api/v1/all_events")
 api.add_resource(GiftApi, "/api/v1/gift")
--- routes/household.py
+++ routes/household.py
@@ -3,68 +3,88 @@ Creates the household-related endpoints.
 """
 
 from logging import getLogger
-from main import app
 from backend import db
 from models.models import Household
 from flask import request
+from flask_restful import Resource, reqparse
 from sqlalchemy.exc import SQLAlchemyError, InvalidRequestError, NoResultFound
+import json
 
 logger = getLogger()
 
-
-@app.route("/api/v1/all_households", methods=['GET'])
-def api_household_all():
-    """Return all households from the database"""
-    logger.debug("Start of HouseholdCollectionAPI.GET")
-    logger.debug(request)
-
-    # Retrieve all households from the db, sorted by id
-    try:
-        households = Household.query.order_by(Household.id).all()
-        logger.info(f"Successfully retrieved data for {households.__len__()} households.")
-
-    except SQLAlchemyError as e:
-        error_msg = f"SQLAlchemyError retrieving data: {e}"
-        logger.info(error_msg)
-        logger.debug("End of HouseholdCollectionAPI.GET")
-        return {'message': 'Error', 'data': error_msg}, 500
-
-    except BaseException as e:
-        error_msg = f"BaseException retrieving data: {e}"
-        logger.info(error_msg)
-        logger.debug("End of HouseholdCollectionAPI.GET")
-        return {'message': 'Error', 'data': error_msg}, 500
-
-    # Compile these data into a list
-    try:
-        output = []
-        for hh in households:
-            output.append(hh.to_dict())
-
-        logger.debug("End of HouseholdAPI.GET")
-        return {'message': 'Success', 'data': output}, 200
-
-    except BaseException as e:
-        error_msg = f"Error compiling data into a list of `dict` to return: {e}"
-        logger.info(error_msg)
-        logger.debug("End of HouseholdCollectionAPI.GET")
-        return {'message': 'Error', 'data': error_msg}, 500
+# Initialize a parser for the request parameters
+parser = reqparse.RequestParser(trim=True)
 
 
-@app.route("/api/v1/household", methods=['GET', 'POST', 'PUT', 'DELETE'])
-def api_household_one():
-    logger.debug(f"Start of HouseholdAPI.{request.method.__str__()}")
-    logger.debug(request)
+class HouseholdCollectionApi(Resource):
+    """
+    Endpoint:   /api/v1/all_households
+    Methods:    GET
+    """
+    @staticmethod
+    def get() -> json:
+        """Return all households from the database"""
+        logger.debug("Start of HouseholdCollectionAPI.GET")
 
-    # Convert data from the request into a local dictionary
-    form_data = request.get_json(force=True)
-    logger.debug(f"Request json: {form_data}")
+        # Retrieve all households from the db, sorted by id
+        try:
+            households = Household.query.order_by(Household.id).all()
+            logger.info(f"Successfully retrieved data for {households.__len__()} households.")
 
-    if request.method == "GET":
+        except SQLAlchemyError as e:
+            error_msg = f"SQLAlchemyError retrieving data: {e}"
+            logger.info(error_msg)
+            logger.debug("End of HouseholdCollectionAPI.GET")
+            return error_msg, 500
+
+        except BaseException as e:
+            error_msg = f"BaseException retrieving data: {e}"
+            logger.info(error_msg)
+            logger.debug("End of HouseholdCollectionAPI.GET")
+            return error_msg, 500
+
+        # Compile these data into a list
+        try:
+            output = []
+            for hh in households:
+                output.append(hh.to_dict())
+
+            logger.debug("End of HouseholdAPI.GET")
+            return output, 200
+
+        except BaseException as e:
+            error_msg = f"Error compiling data into a list of `dict` to return: {e}"
+            logger.info(error_msg)
+            logger.debug("End of HouseholdCollectionAPI.GET")
+            return error_msg, 500
+
+
+class HouseholdApi(Resource):
+    """
+    Endpoint:   /api/v1/household
+    Methods:    GET, POST, PUT, DELETE
+    """
+
+    @staticmethod
+    def get() -> json:
+        logger.debug("Start of HouseholdAPI.GET")
+        logger.debug(request)
+
+        # Parse the request data to a local dictionary
+        # request_data = request.get_json(force=True)
+        # logger.debug(f"Request json: {request_data}")
+
+        # The only argument expected is `id`
+        parser.add_argument("id", type=int, nullable=False, store_missing=False,
+                            required=True)
+
+        # Parse the arguments provided
+        args = parser.parse_args()
+
         """Return data for the specified household_id"""
         # Validate that a household id was provided
         try:
-            household_id = form_data.get('id')
+            household_id = args['id']
             logger.debug(f"Household with id={household_id} was read successfully")
         except KeyError as e:
             logger.info(f"Error parsing household id: no value was provided. {e}")
@@ -79,31 +99,46 @@ def api_household_one():
                 logger.info(f"Found the requested household!")
                 logger.debug(f"Household: {household.to_dict()}")
                 logger.debug("End of HouseholdAPI.GET")
-                return {"message": "Success", "data": household.to_dict()}, 200
+                return household.to_dict(), 200
             else:
                 # No record with this id exists in the db
                 logger.info(f"No household found with id={household_id}.")
                 logger.debug("End of HouseholdAPI.GET")
-                return {
-                           "message": "Error",
-                           "data":    f"No household found with id={household_id}."
-                       }, 404
+                return f"No household found with id={household_id}.", 404
 
         except (InvalidRequestError, NoResultFound, AttributeError) as e:
             error_msg = f"No household found with id={household_id}.\n{e}"
             logger.info(error_msg)
             logger.debug(f"End of HouseholdAPI.GET")
-            return {'message': 'Error', 'data': error_msg}, 404
+            return error_msg, 404
 
-    elif request.method == "POST":
+    @staticmethod
+    def post() -> json:
         """Add a new household record to the database"""
+
+        # Define the expected arguments
+        parser.add_argument("nickname", type=str)
+        parser.add_argument("first_names", type=str)
+        parser.add_argument("surname", type=str)
+        parser.add_argument("formal_name", type=str)
+        parser.add_argument("relationship", type=str)
+        parser.add_argument("relationship_type", type=str)
+        parser.add_argument("family_side", type=str)
+        parser.add_argument("kids", type=str)
+        parser.add_argument("pets", type=str)
+        parser.add_argument("should_receive_holiday_card", type=int)
+        parser.add_argument("notes", type=str)
+
+        # Parse the arguments provided
+        args = parser.parse_args()
+
         # Create a new Household record using the provided data
         try:
             logger.debug(f"Attempting to create a Household from the provided data.")
-            new_household = Household(**form_data)
+            new_household = Household(**args)
             db.session.add(new_household)
             db.session.flush()
-            logger.info(f"New record flushed: {new_household.to_dict()}")
+            logger.info(f"New record flushed has id={new_household.id}")
             logger.debug(f"Added new household record to the db session")
 
             # Commit this new record so the db generates an id
@@ -114,59 +149,88 @@ def api_household_one():
 
             # Return the household_id to the requester
             logger.debug("End of HouseholdAPI.POST")
-            return {'message': 'Success', 'data': new_household.id}, 201
+            return new_household.id, 201
 
         except SQLAlchemyError as e:
             error_msg = f"Unable to create a new Household record.\n{e}"
             logger.debug(error_msg)
             logger.debug("End of HouseholdAPI.POST")
-            return {"message": "Error", "data": error_msg}, 500
+            return error_msg, 500
 
-    # elif request.method == "PUT":
-    #     """Update an existing record by household_id"""
-    # Validate that a household_id was provided
-    #     try:
-    #         household_id = args["households"]
-    #         logger.debug(f"Household_id={household_id} was read successfully")
-    #     except KeyError as e:
-    #         logger.info(f"Error parsing household_id: no value was provided. {e}")
-    #         return {"message": "Error", "data": "Must provide a value for household_id."}, 400
-    #
-    #     try:
-    #         # Retrieve the specified household record
-    #         household = Household.query.get(household_id)
-    #
-    #         # Update this record with the provided data
-    #         household.nickname = args["nickname"]
-    #         household.surname = args["surname"]
-    #         household.formal_name = args["formal_name"]
-    #         household.relationship = args["relationship"]
-    #         household.relationship_type = args["relationship_type"]
-    #
-    #         # Commit these changes to the db
-    #         logger.debug("Attempting to commit db changes")
-    #         db.session.commit()
-    #         logger.info("Changes saved to the database")
-    #
-    #         logger.debug("End of HouseholdAPI.PUT")
-    #         return household.id, 200
-    #
-    #     except SQLAlchemyError as e:
-    #         error_msg = f"Unable to update Household record.\n{e}"
-    #         logger.debug(error_msg)
-    #         logger.debug("End of HouseholdAPI.PUT")
-    #         return error_msg, 500
+    @staticmethod
+    def put() -> json:
+        """Update an existing record by household_id"""
+        # Define the expected arguments
+        parser.add_argument("id", type=int, nullable=False, store_missing=False,
+                            required=True)
+        parser.add_argument("nickname", type=str)
+        parser.add_argument("first_names", type=str)
+        parser.add_argument("surname", type=str)
+        parser.add_argument("formal_name", type=str)
+        parser.add_argument("relationship", type=str)
+        parser.add_argument("relationship_type", type=str)
+        parser.add_argument("family_side", type=str)
+        parser.add_argument("kids", type=str)
+        parser.add_argument("pets", type=str)
+        parser.add_argument("should_receive_holiday_card", type=int)
+        parser.add_argument("notes", type=str)
 
-    elif request.method == "DELETE":
+        # Parse the arguments provided
+        args = parser.parse_args()
+
+        # Validate that a household_id was provided
+        try:
+            household_id = args["id"]
+            logger.debug(f"Household_id={household_id} was read successfully")
+        except KeyError as e:
+            logger.info(f"Error parsing household_id: no value was provided. {e}")
+            return "Must provide a value for household_id.", 400
+
+        try:
+            # Retrieve the specified household record
+            household = Household.query.get(household_id)
+
+            # Update this record with the provided data
+            household.nickname = args["nickname"]
+            household.surname = args["surname"]
+            household.formal_name = args["formal_name"]
+            household.relationship = args["relationship"]
+            household.relationship_type = args["relationship_type"]
+
+            # Commit these changes to the db
+            logger.debug("Attempting to commit db changes")
+            db.session.commit()
+            logger.info("Changes saved to the database")
+
+            logger.debug("End of HouseholdAPI.PUT")
+            return household.id, 200
+
+        except SQLAlchemyError as e:
+            error_msg = f"Unable to update Household record.\n{e}"
+            logger.debug(error_msg)
+            logger.debug("End of HouseholdAPI.PUT")
+            return error_msg, 500
+
+    @staticmethod
+    def delete() -> json:
         """Delete the specified record by household_id"""
+
+        # The only argument expected is `id`
+        parser.add_argument("id", type=int, nullable=False, store_missing=False,
+                            required=True)
+
+        # Parse the arguments provided
+        args = parser.parse_args()
+
         # Validate that a household id was provided
         try:
-            household_id = form_data.get('id')
+            household_id = args['id']
             logger.debug(f"Household id={household_id} was provided.")
         except KeyError as e:
-            logger.info(f"Missing household id. {e}")
+            error_msg = f"Missing household id.\n{e}"
+            logger.info(error_msg)
             logger.debug(f"End of HouseholdAPI.DELETE")
-            return {"message": "Error", "data": f"Missing household id. {e}"}, 400
+            return error_msg, 400
 
         try:
             # Retrieve the selected record
@@ -183,19 +247,16 @@ def api_household_one():
                 logger.info("Household record successfully deleted.")
 
                 logger.debug("End of HouseholdAPI.GET")
-                return {
-                           "message": "Success",
-                           "data":    f"Successfully deleted household id: {household_id}"
-                       }, 200
+                return f"Successfully deleted household id: {household_id}", 200
             else:
                 # No record with this id exists in the db
                 error_msg = f"No household found with id={household_id}."
                 logger.debug(error_msg)
                 logger.debug("End of HouseholdAPI.GET")
-                return {"message": "Error", "data": error_msg}, 404
+                return error_msg, 404
 
         except (InvalidRequestError, NoResultFound, AttributeError) as e:
             error_msg = f"No household found with id={household_id}.\n{e}"
             logger.debug(error_msg)
             logger.debug(f"End of HouseholdAPI.GET")
-            return {"message": "Error", "data": error_msg}, 404
+            return error_msg, 404
